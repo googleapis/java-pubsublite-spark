a5f0759e395cba64ba133a1161d954be3eea5812
Michael Jiang <jiangmichael@google.com>
2021-01-27 15:24:00 -0500

    update

---
+++ samples/snapshot/src/main/java/pubsublite/spark/WordCount.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pubsublite.spark;
+
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.types.DataTypes;
+
+import static org.apache.spark.sql.functions.split;
+
+public class WordCount {
+
+    public static void main(String[] args) throws Exception {
+
+        SparkSession spark = SparkSession.builder()
+                .appName("Word count")
+                .getOrCreate();
+
+        Dataset<Row> df = spark.readStream()
+                .format("pubsublite")
+                .option("pubsublite.subscription", args[0])
+                .load();
+
+        Column splitCol = split(df.col("data"), "_");
+
+        df = df.withColumn("word", splitCol.getItem(0))
+                .withColumn("word_count", splitCol.getItem(1).cast(DataTypes.LongType));
+
+        df = df.groupBy("word").sum("word_count");
+        df = df.orderBy(df.col("sum(word_count)").desc()).cache();
+
+        StreamingQuery query = df.writeStream()
+                .format("console")
+                .start();
+
+        Thread.sleep(10000);
+        query.stop();
+    }
+}
