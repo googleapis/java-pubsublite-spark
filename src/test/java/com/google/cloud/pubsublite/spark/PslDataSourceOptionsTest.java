103e1ebf740929399677338a216ea5f187d3e4aa
jiangmichaellll <40044148+jiangmichaellll@users.noreply.github.com>
2021-02-22 18:03:33 -0500

    fix: Better error message when subscription path is invalid. (#87)

---
+++ src/test/java/com/google/cloud/pubsublite/spark/PslDataSourceOptionsTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.pubsublite.spark;
+
+import static org.junit.Assert.assertThrows;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.spark.sql.sources.v2.DataSourceOptions;
+import org.junit.Test;
+
+public class PslDataSourceOptionsTest {
+
+  @Test
+  public void testInvalidSubPath() {
+    DataSourceOptions options =
+        new DataSourceOptions(ImmutableMap.of(Constants.SUBSCRIPTION_CONFIG_KEY, "invalid/path"));
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> PslDataSourceOptions.fromSparkDataSourceOptions(options));
+  }
+}
